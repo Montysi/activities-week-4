4abd0e6911deee18a4ca866b64b8213fd88aa900
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-10 12:19:17 +0100

    day1 activity 2 complete

---
+++ day1/activity1.js
@@ -0,0 +1,19 @@
+// const greeting = () => {
+//     console.log("Hello Codenation")
+// }
+// greeting()
+
+
+
+const greeting = (greet) => {
+  console.log("Hello Codenation");
+};
+// const  = () => {
+//     for (let i = 0; i < greeting.length; i++);
+// }
+// greeting();
+
+
+
+console.log(greetingCodenation)
+
---
+++ day1/activity2.js
@@ -0,0 +1,5 @@
+myArr = [5, 10, 15, 20, 25];
+
+const multiplyArr = myArr.map(num => num * 3);
+
+console.log(multiplyArr);
